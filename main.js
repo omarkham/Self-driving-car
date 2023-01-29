1143a608a575222b5efc5261c0a439b5a174fee0
Olivia <95939886+omarkham@users.noreply.github.com>
2023-01-28 22:17:11 -0500

    Update main.js
    Added  code for the new buttons, outlined the leaderboard necessary functions

--- main.js
+++ main.js
@@ -17,34 +17,64 @@ function stopTimer() {
     this.elapsedTime = this.endTime - this.startTime;
     return this.elapsedTime;
 }
+function resetAll() {
+    location.reload();
+}
+
 startTimer();
 
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
+const numberOfCarSelect = document.getElementById("numberOfCar");
+const mutationRatioSelect = document.getElementById("mutationRatio");
+const numberOfTrafficSelect = document.getElementById("numberOfTraffic");
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=200;//this is the multiple different blue car possibilities
+let N=200;//this is the multiple different blue car possibilities
+let M = 200;//amount of traffic
+let mutationRatio = 0.1;
+mutationRatioSelect.value = mutationRatio;
+numberOfCarSelect.value = N;
+numberOfTrafficSelect.value = M;
+
+
+if (localStorage.getItem("numberOfCar")) {
+    N = Number(localStorage.getItem("numberOfCar"));
+    numberOfCarSelect.value = N;
+}
+
+if (localStorage.getItem("numberOfTraffic")) {
+    M = Number(localStorage.getItem("numberOfTraffic"));
+    numberOfTrafficSelect.value = M;
+}
+
+if (localStorage.getItem("mutationRatio")) {
+    mutationRatio = Number(localStorage.getItem("mutationRatio"));
+    mutationRatioSelect.value = mutationRatio;
+}
+
 const cars = generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
     for(let i=0;i<cars.length;i++){
         cars[i].brain=JSON.parse(
             localStorage.getItem("bestBrain"));
+        //The first car (best car) will remain the same, but the other ones will be mutated
         if(i!=0){
-            NeuralNetwork.mutate(cars[i].brain,0.1);
+            NeuralNetwork.mutate(cars[i].brain, mutationRatio);
         }
     }
 }
 
 //Writing a function to generate traffic automatically (not hardcoding the cars)
 const traffic = [];
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < M; i++) {
     //Math.random() * (max - min) + min
     //constructor(x,y,width,height,controlType,maxSpeed=3,color="blue")
     let x = Math.floor(Math.random() * 2.5);
@@ -84,13 +114,24 @@ function addCarToArray(car) {
 animate();
 
 function save(){
-    localStorage.setItem("bestBrain",
-        JSON.stringify(bestCar.brain));
+    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
     time = stopTimer();
 }
 
 function discard(){
     localStorage.removeItem("bestBrain");
+}
+
+function setNumberOfCar(event) {
+    localStorage.setItem("numberOfCar", event.target.value);
+}
+
+function setNumberOfTraffic(event) {
+    localStorage.setItem("numberOfTraffic", event.target.value);
+}
+
+function setMutationRatio(event) {
+    localStorage.setItem("mutationRatio", event.target.value);
 }
 
 function generateCars(N){
@@ -101,38 +142,88 @@ function generateCars(N){
     return cars;
 }
 
+//work in progress for scoreboard
+
+/*function getCurrentScore(M) {
+    score = M*stopTimer();
+    return score;
+}
+function addScoreToLeaderboard(name, score) {
+    var leaderboard = document.getElementById("leaderboard");
+    var newRow = leaderboard.insertRow(-1);
+    var nameCell = newRow.insertCell(0);
+    var scoreCell = newRow.insertCell(1);
+    nameCell.innerHTML = name;
+    scoreCell.innerHTML = score;
+}
+function submitScore() {
+    var name = prompt("Enter your name:");
+    var score = getCurrentScore(M);
+    addScoreToLeaderboard(name, score);
+}
+function sortLeaderboard() {
+    var leaderboard = document.getElementById("leaderboard");
+    var rows = leaderboard.rows;
+    var sortedRows = [];
+    for (var i = 1; i < rows.length; i++) {
+        sortedRows.push(rows[i]);
+    }
+    sortedRows.sort(function(a, b) {
+        var scoreA = parseInt(a.cells[1].innerHTML);
+        var scoreB = parseInt(b.cells[1].innerHTML);
+        return scoreB - scoreA;
+    });
+    for (var i = 0; i < sortedRows.length; i++) {
+        leaderboard.appendChild(sortedRows[i]);
+    }
+}
+function showLeaderboard() {
+    sortLeaderboard();
+    var leaderboard = document.getElement("div");
+    leaderboard.innerHTML = "Your leaderboard content goes here";
+    leaderboard.id = "leaderboard";
+    document.body.appendChild(leaderboard);
+}
+*/
 function animate(time){
+    //to update each car of traffic according to the road
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
     for(let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
-    bestCar=cars.find(
-        c=>c.y==Math.min(
-            ...cars.map(c=>c.y)
-        ));
+    //will find the minimum y for each generated car and chose it as the best car
+    //(the car that has gone the furthest)
+    bestCar=cars.find(c=>c.y==Math.min(...cars.map(c=>c.y)));
     
+    //to make sure the car doesnt stretch when it moves
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
 
     carCtx.save();
+    //move the canvas with the car(make it appear that a camera follows the car from above)
     carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
 
     road.draw(carCtx);
+    //draw the traffic cars
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx);
     }
+    //makes generation less messy
     carCtx.globalAlpha=0.2;
     for(let i=0;i<cars.length;i++){
+        //draws the main blue cars on canvas
         cars[i].draw(carCtx);
     }
+
     carCtx.globalAlpha=1;
     bestCar.draw(carCtx,true);
 
     carCtx.restore();
 
     networkCtx.lineDashOffset=-time/50;
+    //draws the animated version of the neural network
     Visualizer.drawNetwork(networkCtx,bestCar.brain);
     requestAnimationFrame(animate);
 }
